1a04f05e8ed2370b3cae912c9aed952a663d0404
Dauniusha <daniilegiptsev@gmail.com>
2021-12-26 23:04:20 +0300

    Init: deploy backend

---
+++ dist/src/settings/referenceCurrency.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var REFERENCE_CURRENCY = {
+    Cur_ID: 0,
+    Date: new Date().toISOString(),
+    Cur_Abbreviation: 'BYN',
+    Cur_Scale: 1,
+    Cur_Name: 'Белорусский рубль',
+    Cur_OfficialRate: 1,
+};
+exports.default = REFERENCE_CURRENCY;
+//# sourceMappingURL=referenceCurrency.js.map
